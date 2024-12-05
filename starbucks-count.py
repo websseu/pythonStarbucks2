6b0d1953594385f6baaa887aac72728b9f10bbe2
웹스토리보이 <webstoryboy@naver.com>
2024-12-05 14:31:07 +0900

    2024.12.05

--- starbucks-count.py
+++ starbucks-count.py
@@ -6,12 +6,33 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 import time
+import re
 import os
 import json
 from datetime import datetime
 
 # 현재 날짜
 current_date = datetime.now().strftime("%Y-%m-%d")
+
+# 한글 지역명과 영문 지역명 매핑
+location_name_mapping = {
+    "서울": "seoul",
+    "부산": "busan",
+    "대구": "daegu",
+    "인천": "incheon",
+    "광주": "gwangju",
+    "대전": "daejeon",
+    "울산": "ulsan",
+    "경기": "gyeonggi",
+    "강원": "gangwon",
+    "충북": "chungbuk",
+    "충남": "chungnam",
+    "전북": "jeolbuk",
+    "전남": "jeolnam",
+    "경북": "gyeongbuk",
+    "경남": "gyeongnam",
+    "제주": "jeju"
+}
 
 # 폴더 생성
 base_folder = "location"
@@ -63,7 +84,54 @@ try:
     # 페이지 소스를 BeautifulSoup을 사용하여 저장
     soup = BeautifulSoup(browser.page_source, 'html.parser')
 
-    # 전체 매장 수 추출
+    # 서울  데이터를 저장할 변수
+    store_data = []
+
+    # 서울 데이터 수집
+    stores = soup.select(".quickSearchResultBoxSidoGugun li.quickResultLstCon")
+    for store in stores:
+        # 이름, 주소, 위도, 경도 추출
+        name = store.get("data-name")
+        address = store.select_one(".result_details").text.strip() if store.select_one(".result_details") else None
+        
+        # 주소에서 전화번호 형식을 제거
+        if address:
+            address = re.sub(r'\d{4}-\d{4}', '', address).strip()  # 전화번호 패턴 제거
+        
+        latitude = store.get("data-lat")
+        longitude = store.get("data-long")
+
+        # 수집된 정보를 딕셔너리에 저장
+        store_data.append({
+            "name": name,
+            "address": address,
+            "latitude": latitude,
+            "longitude": longitude
+        })
+    
+    # 지역 이름 정의
+    location_name_kor = "서울"
+    location_name_eng = location_name_mapping[location_name_kor]
+    
+    # 최종 JSON 구조 정의
+    final_data = {
+        "location": "서울",
+        "count": len(store_data),
+        "date": current_date,
+        "item": store_data
+    }
+
+    # location 폴더에 영문 이름으로 폴더 생성
+    location_folder_path = os.path.join(base_folder, location_name_eng)
+    os.makedirs(location_folder_path, exist_ok=True)
+
+    # 데이터 저장
+    file_name = f"{location_folder_path}/{location_name_eng}_{current_date}.json"
+    with open(file_name, 'w', encoding='utf-8') as f:
+        json.dump(final_data, f, ensure_ascii=False, indent=4)
+        print(f"{location_name_eng} 데이터가 '{file_name}' 파일에 저장되었습니다.")
+
+    # 전체 매장 갯수 추출
     total_count_element = soup.select_one(".result_num_wrap .sidoSetResult")
     total_count = int(total_count_element.text.strip()) if total_count_element else 0
     print(f"전체 매장 수: {total_count}")
